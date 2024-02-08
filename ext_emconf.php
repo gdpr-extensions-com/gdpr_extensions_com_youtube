2ab4f04f69cc80d7f215fe68692158b3910321a5
gdpr-extensions-com <support@gdpr-extensions.com>
2024-02-08 20:42:58 +0500

    published version

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'gdpr-extensions-com-youtube-2clicksolution',
-    'description' => 'The GDPR Youtube 2 Click Solution, brought to you by GDPR-Extensions.com, is a feature-rich solution designed to empower website owners in effectively managing YouTube videos and bolstering user privacy.',
+    'description' => 'The GDPR Youtube 2 Click Solution, brought to you by GDPR-Extensions.com, is a feature-rich solution designed to empower website owners in effectively managing Youtube videos and bolstering user privacy.',
     'category' => 'plugin',
     'author' => 'GDPR-Extensions.com Team',
     'author_email' => 'support@gdpr-extensions.com',
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.10',
+            'typo3' => '11.5.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
