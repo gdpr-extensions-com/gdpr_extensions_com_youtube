135ab9475b6627de882388f280672232b839581b
gdpr-extensions.com <151840462+gdpr-extensions-com@users.noreply.github.com>
2024-02-08 19:20:24 +0500

    Updated ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,17 +1,17 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'gdpr-extensions-com-youtube-2clicksolution',
-    'description' => '',
+    'title' => 'gdpr-extensions-com-vimeo-2clicksolution',
+    'description' => 'The GDPR Vimeo 2 Click Solution, brought to you by GDPR-Extensions.com, is a feature-rich solution designed to empower website owners in effectively managing Vimeo videos and bolstering user privacy.',
     'category' => 'plugin',
-    'author' => '',
-    'author_email' => '',
-    'state' => 'alpha',
+    'author' => 'GDPR-Extensions.com Team',
+    'author_email' => 'support@gdpr-extensions.com',
+    'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '1.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '11.5.0-12.4.10',
         ],
         'conflicts' => [],
         'suggests' => [],
